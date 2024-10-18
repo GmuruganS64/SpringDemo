b3bf68187772d37d606fec8b6b57507efc465b1d
GmuruganS64 <sgmurugan27@gmail.com>
2024-10-18 11:41:04 +0530

    Hello World

--- src/main/java/com/example/SpringDemo/SpringDemoApplication.java
+++ src/main/java/com/example/SpringDemo/SpringDemoApplication.java
@@ -8,6 +8,8 @@ public class SpringDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
+		
+	System.out.println("Hello World !!!");
 	}
 
 }
